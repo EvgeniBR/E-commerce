74569ec93cd053385b34bae4119453ee187ec709
EvgeniBR <genib1989@gmail.com>
2021-02-05 16:51:50 +0200

    menu

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './HomePage.styles.scss'
+const HomePage= () =>{
+
+    return(
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <div className='title'>
+                            <h1>Hats</h1>
+                            <span>SHOP NOW</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <div className='title'>
+                            <h1>Jackets</h1>
+                            <span>SHOP NOW</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <div className='title'>
+                            <h1>Sneakers</h1>
+                            <span>SHOP NOW</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <div className='title'>
+                            <h1>Women</h1>
+                            <span>SHOP NOW</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <div className='title'>
+                            <h1>Man</h1>
+                            <span>SHOP NOW</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
