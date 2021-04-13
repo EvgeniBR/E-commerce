17dddaced9dc82438c719036f976d031e0c94248
EvgeniBR <genib1989@gmail.com>
2021-04-13 13:15:53 +0300

    more work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import HomePage from './pages/HomePage/HomePage.component'
-
+import './App.css'
 
 function App() {
   return (
--- src/pages/HomePage/HomePage.component.jsx
+++ src/pages/HomePage/HomePage.component.jsx
@@ -1,50 +1,10 @@
 import React from "react";
+import Directory from "../../components/Directory/Directory.component";
 import "./HomePage.styles.scss";
 const HomePage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <div className="title">
-              <h1>Hats</h1>
-              <span>SHOP NOW!!</span>
-            </div>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <div className="title">
-              <h1>Jackets</h1>
-              <span>SHOP NOW!!</span>
-            </div>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <div className="title">
-              <h1>Sneakers</h1>
-              <span>SHOP NOW!!</span>
-            </div>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <div className="title">
-              <h1>Women</h1>
-              <span>SHOP NOW!!!</span>
-            </div>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <div className="title">
-              <h1>Man</h1>
-              <span>SHOP NOW!!!</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 };
