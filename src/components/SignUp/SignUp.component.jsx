3f222b61c569eed366874a1a6d162cd241f1caf1
EvgeniBR <genib1989@gmail.com>
2021-05-02 12:30:43 +0300

    sign up

---
+++ src/components/SignUp/SignUp.component.jsx
@@ -0,0 +1,121 @@
+import React,{useState} from "react";
+import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+import CustomButton from "../CustomButton/CustomButton.component";
+import FormInput from "../FormInput/FormInput.component";
+
+const SignUp = () => {
+  const [currentState, setCurrentState] = useState({
+    displayName:'',
+    email:'',
+    password:'',
+    confirmPassword:'',
+  });
+
+  const handleSubmit = async(event) =>{
+    event.preventDefault();
+    const { displayName, email, password, confirmPassword } = currentState;
+
+    if(password !== confirmPassword){
+        alert('passwords don\'t match');
+        return;
+    }
+    try{
+        const {user} = await auth.createUserWithEmailAndPassword(email, password)
+        await createUserProfileDocument(user , {displayName});
+        setCurrentState({
+            displayName:'',
+            email:'',
+            password:'',
+            confirmPassword:'',  
+        })
+    }catch(e){
+        console.log('error in handle submit of sign up' ,e);
+    }
+  }
+
+  const handleChange = (event) =>{
+    const {name, value} = event.target
+    switch (name){
+        case 'displayName':
+            setCurrentState({
+                displayName:value,
+                email:currentState.email,
+                password:currentState.password,
+                confirmPassword:currentState.confirmPassword,  
+            })
+            break
+        case 'email':
+            setCurrentState({
+                displayName:currentState.displayName,
+                email:value,
+                password:currentState.password,
+                confirmPassword:currentState.confirmPassword,  
+            })
+            break 
+        case 'password':
+            setCurrentState({
+                displayName:currentState.displayName,
+                email:currentState.email,
+                password:value,
+                confirmPassword:currentState.confirmPassword,  
+            })
+            break 
+        case 'confirmPassword':
+            setCurrentState({
+                displayName:currentState.displayName,
+                email:currentState.email,
+                password:currentState.password,
+                confirmPassword:value,  
+            })
+            break
+            default:
+                console.log( "No value found"); 
+    }
+  }
+
+  const { displayName, email, password, confirmPassword } = currentState;
+
+  return (
+    <div className="sign-up">
+      <h2 className="title">I do not have an account</h2>
+      <span>Sign Up with email & password</span>
+      <form className="sign-up-form" onSubmit={(event) => handleSubmit(event)}>
+        <FormInput
+          type="text"
+          name="displayName"
+          value={displayName}
+          onChange={(event) => handleChange(event)}
+          label="Display Name"
+          required
+        />
+        <FormInput
+          type="email"
+          name="email"
+          value={email}
+          onChange={(event) => handleChange(event)}
+          label="Email"
+          required
+        />
+        <FormInput
+          type="password"
+          name="password"
+          value={password}
+          onChange={(event) => handleChange(event)}
+          label="Password"
+          required
+        />
+        <FormInput
+          type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={(event) => handleChange(event)}
+          label="Confirm Password"
+          required
+        />
+        <CustomButton type='submit'>SIGN UP</CustomButton>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
--- src/pages/SignInSignUp/SignInSignUp.component.jsx
+++ src/pages/SignInSignUp/SignInSignUp.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SignIn from '../../components/SignIn/SignIn.component'
+import SignUp from '../../components/SignUp/SignUp.component'
 
 import './SignInSignUp.styles.scss'
 
@@ -8,6 +9,7 @@ const SignInSignUp = () =>{
     return (
         <div className="sign-in-sign-up">
             <SignIn />
+            <SignUp />
         </div>
     )
 
