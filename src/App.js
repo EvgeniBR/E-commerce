d75abaae75156051e047e9df901ac8a65f9c567b
EvgeniBR <genib1989@gmail.com>
2021-05-02 11:12:13 +0300

    firebase basics

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import ShopPage from "./pages/ShopPage/ShopPage.component";
 import Header from "./components/Header/Header.component";
 import SignInSignUp from "./pages/SignInSignUp/SignInSignUp.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth , createUserProfileDocument } from "./firebase/firebase.utils";
 
 
 class App extends React.Component {
@@ -20,10 +20,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount = () =>{
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
-      console.log(user)
-      this.setState({currentUser:user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
+     if(userAuth){
+       const userRef = await createUserProfileDocument(userAuth)
+
+       userRef.onSnapshot(snapShot =>{
+         this.setState({
+           currentUser:{
+             id:snapShot.id,
+             ...snapShot.data()
+           }
+         },()=>console.log(this.state))
+       });
+     }else{
+       this.setState({currentUser:userAuth})
+     }
     });
+    
   }
 
   componentWillUnmount = () =>{
